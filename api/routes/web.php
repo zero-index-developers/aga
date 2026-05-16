6d82d95a2923e504daded34bc6b4f45441bdbe16
1101101011 <ilardecharles@gmail.com>
2026-05-17 02:31:48 +0800

    feat: added scribe and scalar for api testing and added flake.nix for my dev env

--- api/routes/web.php
+++ api/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/api/', function () {
     return view('welcome');
+});
+
+Route::get('/api/hello', function () {
+    return response()->json(['message' => 'Hello, World!']);
 });
